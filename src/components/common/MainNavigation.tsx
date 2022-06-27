00cc53e5919bdf87be34bcb65ad498197607fe9c
Sebastian Key <sebastianwkey@gmail.com>
2022-06-27 14:38:18 +1000

    Cleanup

--- src/components/common/MainNavigation.tsx
+++ src/components/common/MainNavigation.tsx
@@ -43,9 +43,9 @@ interface NavLinkProps {
 
 const NavLink: FC<NavLinkProps> = ({ label, hideLabel = false, icon, url }) => {
   const router = useRouter();
-  const active =
-    !isExternalUrl(url) &&
-    url.replace('/', '').includes(router.pathname.split('/')[1]);
+  const targetUrl = url.replace('/', '');
+  const currentTopLevelUrl = router.pathname.split('/')[1];
+  const active = !isExternalUrl(url) && targetUrl.includes(currentTopLevelUrl);
 
   return (
     <Link href={url}>
